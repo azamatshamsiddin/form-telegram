9d05eb29267420ae5cd38d6424c4137077ee62e2
azamatshamsiddin <muratovichazamat@yandex.ru>
2022-05-06 11:25:11 +0500

    fix

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ form.addEventListener("submit", (e) => {
 	
 	const TOKEN_BOT = "5342053813:AAF_CBROYfiihIcvnt0EK-cNFXq6Oe4lPBc"
 	
-	let fullName = document.querySelector(".form-fullname").value
+	let fullName = document.querySelector(".form-fullname")
+	let fullNameVal = fullName.value
 	let phone = document.querySelector(".form-phone").value
 	let description = document.querySelector(".form-textArea").value
 	console.log(description)
-	let my_text = `Result is:%0A - <b>User fullname: </b> ${fullName}. %0A - <b>User phone number: </b> ${phone}. %0A - <b>User message: </b> ${description}.`;
+	let my_text = `Result is:%0A - <b>User fullname: </b> ${fullNameVal}. %0A - <b>User phone number: </b> ${phone}. %0A - <b>User message: </b> ${description}.`;
 	const chat_id = -1001317139562;
 	let url = `https://api.telegram.org/bot${TOKEN_BOT}/sendMessage?chat_id=${chat_id}&text=${my_text}&parse_mode=html`
 	fullName.innerHTML = null
