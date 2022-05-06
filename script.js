6cca0917a88af0520f4a54e9b0aa2753adbd7235
azamatshamsiddin <muratovichazamat@yandex.ru>
2022-05-06 11:10:11 +0500

    re-styling

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ form.addEventListener("submit", (e) => {
 	
 	const TOKEN_BOT = "5342053813:AAF_CBROYfiihIcvnt0EK-cNFXq6Oe4lPBc"
 	
-	let email = document.querySelector(".form-email").value
-	let text = document.querySelector(".form-text1").value
-	let my_text = `Result is:%0A - <b>Email: </b> ${email}. %0A - <b>Text: </b> ${text}.`;
+	let fullName = document.querySelector(".form-fullname").value
+	let phone = document.querySelector(".form-phone").value
+	let description = document.querySelector(".form-textArea").value
+	let my_text = `Result is:%0A - <b>User fullname: </b> ${fullName}. %0A - <b>User phone number: </b> ${phone}. %0A - <b>User message: </b> ${description}.`;
 	const chat_id = -1001317139562;
 	let url = `https://api.telegram.org/bot${TOKEN_BOT}/sendMessage?chat_id=${chat_id}&text=${my_text}&parse_mode=html`
 	
 	let api = new XMLHttpRequest()
-	
 	api.open("GET", url, true)
 	api.send();
 	
